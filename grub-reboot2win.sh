2fc2c7e3ee0f08c2afc59960fc41b75854841ea8
kaipee <kaipee@kaipee.co.uk>
2018-02-18 01:49:07 +0000

    added author

--- grub-reboot2win.sh
+++ grub-reboot2win.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Author: Keith Patton 2017
+# Brief: A BASH script to launch a GTK+ prompting to confirm
+# a reboot to Windows installed on a separate partition
+
 zenity --question --text="Reboot to Win7?" --icon-name=windows && sudo grub-reboot 4 && shutdown -r now
